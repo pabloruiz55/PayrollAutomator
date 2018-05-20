8c32941e59a4a715de1b2d05f5cb54f4efd13060
Pablo Ruiz <pabloruiz55@gmail.com>
2018-05-19 21:24:23 -0300

    Added SchedulerInterface.sol

---
+++ SchedulerInterface.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.21;
+
+/**
+ * @title SchedulerInterface
+ * @dev The base contract that the higher contracts: BaseScheduler, BlockScheduler and TimestampScheduler all inherit from.
+ */
+contract SchedulerInterface {
+    function schedule(address _toAddress, bytes _callData, uint[8] _uintArgs)
+        public payable returns (address);
+    function computeEndowment(uint _bounty, uint _fee, uint _callGas, uint _callValue, uint _gasPrice)
+        public view returns (uint);
+}
